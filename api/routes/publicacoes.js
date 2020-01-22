67ecfb232f59b1ef1476a4881229a8c99a3eb67f
PedroFreitas90 <pedrofreitas1410@gmail.com>
2020-01-22 22:50:39 +0000

    add api pubs route

---
+++ api/routes/publicacoes.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var Pubs = require('../controllers/publicacoes')
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  if(req.query.autor){
+    Pubs.filtrarAutor(req.query.autor)
+      .then(dados => res.jsonp(dados))
+      .catch(e => res.status(500).jsonp(e))
+  }
+  else{
+    Pubs.listar()
+      .then(dados => res.jsonp(dados))
+      .catch(e => res.status(500).jsonp(e))
+  } 
+});
+
+router.get('/:id', function(req, res) {
+  Pubs.consultar(req.params.id)
+    .then(dados => res.jsonp(dados))
+    .catch(e => res.status(500).jsonp(e))
+});
+
+module.exports = router;
