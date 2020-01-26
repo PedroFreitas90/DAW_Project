01054e7402f6d77174ec7a385efac75ae62f06cb
PedroFreitas90 <pedrofreitas1410@gmail.com>
2020-01-26 15:27:31 +0000

    ISN rota de posts num grupo

--- ISN/routes/publicacoes.js
+++ ISN/routes/publicacoes.js
@@ -111,17 +111,50 @@ router.post('/comentario',upload.array('ficheiro'),verificaAutenticacao,function
 
 })
     
+router.post('/:idGrupo',upload.array('ficheiro'),verificaAutenticacao,function(req,res){
+  var ficheirosArray = []
 
-    function verificaAutenticacao(req,res,next){
-        if(req.isAuthenticated()){
-        //req.isAuthenticated() will return true if user is logged in
-          next();
-        } else{
-          res.redirect("/login");}
-      }
+  for(var i = 0; i < req.files.length; i++){
+    let oldPath = __dirname + '/../' + req.files[i].path
+    let newPath = __dirname + '/../public/ficheiros/' + req.files[i].originalname
+    console.log("cheguei aqui ")      
+    fs.rename(oldPath,newPath,function(err){
+      if(err) throw err
+    })
+    let novoFicheiro = new Ficheiro({
+      name: req.files[i].originalname,
+      mimetype:req.files[i].mimetype,
+      size: req.files[i].size  
+    })
+    ficheirosArray.push(novoFicheiro)
+  }
+  
+  axios.post('http://localhost:5003/publicacoes', {
+      user_id : req.user.numAluno,
+      titulo : req.body.titulo,
+      text : req.body.texto,
+      ficheiros : ficheirosArray,
+      group : req.body.grupo,
+      group_id: req.params.idGrupo,
+      marcadores : hashtags.filtraHashtags(req.body.texto)
+    })
+
+    .then(dados => res.redirect('/grupos/:idGrupo'))
+    .catch(e=>res.render('error',{error:e}))
+
+
+})
+
+function verificaAutenticacao(req,res,next){
+  if(req.isAuthenticated()){
+    //req.isAuthenticated() will return true if user is logged in
+    next();
+  } else{
+  res.redirect("/login");}
+}
       
 
 
 
 
-      module.exports = router;      
+module.exports = router;      
