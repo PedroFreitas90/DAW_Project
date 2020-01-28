20281fd902a5ff8056aa8a15ac2f1d725755020f
Francisco Saraiva <francisco_saraiva94@hotmail.com>
2020-01-28 10:30:18 +0000

    likes a funcionar

--- ISN/public/scripts/likes.js
+++ ISN/public/scripts/likes.js
@@ -3,20 +3,28 @@ function darGostoPublicacao(idPub) {
         idPublicacao: idPub
     })
         .then(dados => {
-            var elem_class = $('#like' + idPub).attr('class');
-            switch (elem_class) {
-                case "far fa-heart coracao":
-                    $('#like' + idPub).addClass('fas').removeClass('far');
-                    var count = document.getElementById(idPub).nextSibling.data.trim();
-                    count + 1;
-                    document.getElementById(idPub).nextSibling.data = ' ' + count;
-                    return;
-                case "fas fa-heart coracao":
-                    $('#like' + idPub).addClass('far').removeClass('fas');
-                    var count = document.getElementById(idPub).nextSibling.data.trim();
-                    count - 1;
-                    document.getElementById(idPub).nextSibling.data = ' ' + count;
-                    return;
+            var element = document.getElementById('like' + idPub).classList;
+
+            if (element.contains('fas')) {
+                document.getElementById('like' + idPub).classList.remove('fas');
+                document.getElementById('like' + idPub).classList.add('far');
+
+                var count = document.getElementById('like' + idPub).nextSibling.data.trim();
+                count = parseInt(count);
+                count = count - 1;
+                document.getElementById('like' + idPub).nextSibling.data = ' ' + count;
+                return;
+            }
+
+            if (element.contains('far')) {
+                document.getElementById('like' + idPub).classList.remove('far');
+                document.getElementById('like' + idPub).classList.add('fas');
+
+                var count = document.getElementById('like' + idPub).nextSibling.data.trim();
+                count = parseInt(count);
+                count = count + 1;
+                document.getElementById('like' + idPub).nextSibling.data = ' ' + count;
+                return;
             }
         })
         .catch(e => res.render('error', { error: e }))
